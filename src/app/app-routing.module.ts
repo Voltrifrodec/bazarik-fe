6022ba4eb3c62427f0ce82a4de49ebd2d462df24
V0ltaroo <nendorans@gmail.com>
2023-04-01 22:57:22 +0200

    [feature]: Categories Walking Skeleton Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent
+	},
+	{
+		path: 'categories',
+		pathMatch: 'full',
+		component: CategoriesComponent
 	},
 	{
 		path: '**',
