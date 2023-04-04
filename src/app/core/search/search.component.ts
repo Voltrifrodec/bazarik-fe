47859811e758de12543176ae0bb526c3c4200bc3
V0ltaroo <nendorans@gmail.com>
2023-04-04 19:46:52 +0200

    [fix]: Overall Task Finish

--- src/app/core/search/search.component.ts
+++ src/app/core/search/search.component.ts
@@ -7,5 +7,7 @@ import * as fa from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-    fa = fa
+    fa = fa;
+
+    math = Array(150).map((num, i) => num[i] = Math.round(Math.random()));
 }
