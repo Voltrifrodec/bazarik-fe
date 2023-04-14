25d70ddb72850d0ab4ea0e6c3bcf86b4b7cc80bc
V0ltaroo <nendorans@gmail.com>
2023-04-14 22:05:15 +0200

    temp

--- src/app/common/model/subsubcategory.model.ts
+++ src/app/common/model/subsubcategory.model.ts
@@ -3,5 +3,5 @@ import { Subcategory } from "./subcategory.model";
 export interface Subsubcategory {
 	id: number;
 	name: string;
-	subcategory: Subcategory;
+	subcategory: Subcategory | undefined;
 }
--- src/app/subsubcategory/subsubcategory.component.ts
+++ src/app/subsubcategory/subsubcategory.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import * as fa from '@fortawesome/free-solid-svg-icons';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { Advert } from '../common/model/advert.model';
+import { Subsubcategory } from '../common/model/subsubcategory.model';
+import { AdvertService } from '../common/service/advert.service';
+import { SubcategoryService } from '../common/service/subcategory.service';
+import { SubsubcategoryService } from '../common/service/subsubcategory.service';
 
 @Component({
   selector: 'app-subsubcategory',
   templateUrl: './subsubcategory.component.html',
   styleUrls: ['./subsubcategory.component.css']
 })
-export class SubsubcategoryComponent {
-  fa = fa;
+export class SubsubcategoryComponent implements OnInit {
+    fa = fa;
+
+    subsubcategoryId: number;
+    subsubcategory?: Subsubcategory;
+
+    adverts?: Advert[];
+
+    constructor(private subsubcategoryService: SubsubcategoryService, private advertService: AdvertService, private route: ActivatedRoute) {
+        this.subsubcategoryId = this.route.snapshot.params['subcategoryId'];
+        this.subsubcategoryService.getSubsubcategoryById(this.subsubcategoryId).pipe(untilDestroyed(this)).subscribe((subsubcategory: Subsubcategory) => {
+            this.subsubcategory = subsubcategory;
+        });
+        console.log('This subsubcategory has ID:', this.subsubcategoryId);
+    }
+
+
+    getAdverts(): void {
+        this.advertService.getAllAdvertsBySubsubcategoryId(this.subsubcategoryId).pipe(untilDestroyed(this)).subscribe((adverts: Advert[]) => {
+            this.adverts = adverts;
+            console.log('Received adverts for the subsubcategory:', this.adverts);
+        });
+    }
+
+    ngOnInit(): void {
+        this.getAdverts();
+        this.subsubcategoryService.getSubsubcategoryById(this.subsubcategoryId).pipe(untilDestroyed(this)).subscribe((subsubcategory: Subsubcategory) => {
+            this.subsubcategory = subsubcategory;
+        });
+    }
+
 }
