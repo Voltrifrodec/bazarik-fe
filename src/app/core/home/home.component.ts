4cb22436232402118caf74bf8a0e34d81535b80c
V0ltaroo <nendorans@gmail.com>
2023-04-19 21:22:16 +0200

    [feature]: Recent Adverts First Code Upload

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as fa from '@fortawesome/free-solid-svg-icons';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Advert } from 'src/app/common/model/advert.model';
 import { Category } from 'src/app/common/model/category.model';
+import { AdvertService } from 'src/app/common/service/advert.service';
 import { CategoryService } from 'src/app/common/service/category.service';
 
 @UntilDestroy()
@@ -13,21 +15,30 @@ import { CategoryService } from 'src/app/common/service/category.service';
 export class HomeComponent implements OnInit {
   fa = fa;
 
-  constructor(private service: CategoryService) {
+  constructor(private service: CategoryService, private advertService: AdvertService) {
     this.getCategories();
+    this.getRecentAdverts();
   }
 
   categories?: Category[];
+  adverts?: Advert[];
 
   getCategories() : void {
     this.service.getAllCategories().pipe(untilDestroyed(this)).subscribe((categories: Category[]) => {
         this.categories = categories;
-        // console.log(this.categories);
+    });
+  }
+
+  getRecentAdverts() : void {
+    this.advertService.getAllAdverts().pipe(untilDestroyed(this)).subscribe((adverts: Advert[]) => {
+        this.adverts = adverts.slice(-4);
+        console.log(this.adverts);
     });
   }
 
   ngOnInit(): void {
       this.getCategories();
+      this.getRecentAdverts();
   }
 
 }
--- src/app/core/home/recent-advert-list/recent-advert-list.component.ts
+++ src/app/core/home/recent-advert-list/recent-advert-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Advert } from 'src/app/common/model/advert.model';
 
 @Component({
   selector: 'app-recent-advert-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recent-advert-list.component.css']
 })
 export class RecentAdvertListComponent {
+    MilisToDate(arg0: number | undefined | Date) {
+        if(arg0 !== undefined) {
+            return new Date(arg0);
+        }
+        throw new Error('Neplatný vstupný parameter!');
+    }
     
+    @Input()
+    adverts?: Advert[];
+
 }
