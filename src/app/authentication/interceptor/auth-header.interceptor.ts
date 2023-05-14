b2d02984db125ad08e896f17cea684c83e371c5f
VOJAKIS <valastan1@gmail.com>
2023-05-14 13:57:35 +0200

    Formatting

--- src/app/authentication/interceptor/auth-header.interceptor.ts
+++ src/app/authentication/interceptor/auth-header.interceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../common/service/auth.service';
@@ -11,19 +11,19 @@ import { AuthService } from '../../common/service/auth.service';
 @Injectable()
 export class AuthHeaderInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+	constructor(private auth: AuthService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const exclude = 'api/token';
-    if(request.url.search(exclude) === -1) {
-      const token = this.auth.getToken();
-      if(token !== null) {
-          request = request.clone({
-            headers: request.headers.append('Authorization', token)
-          });
-      }
-    }
-    return next.handle(request);
-  }
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		const exclude = 'api/token';
+		if (request.url.search(exclude) === -1) {
+			const token = this.auth.getToken();
+			if (token !== null) {
+				request = request.clone({
+					headers: request.headers.append('Authorization', token)
+				});
+			}
+		}
+		return next.handle(request);
+	}
 }
 
