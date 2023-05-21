af00d95269c1dd46f4dec528478ca6ddedfe22b2
VOJAKIS <valastan1@gmail.com>
2023-05-21 19:25:27 +0200

    Refactored login page logic

--- src/app/authentication/login-page/login-page.component.ts
+++ src/app/authentication/login-page/login-page.component.ts
@@ -3,7 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Auth } from '../../common/model/auth.model';
 import { AuthService } from '../../common/service/auth.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
 	selector: 'app-login-page',
 	templateUrl: './login-page.component.html',
@@ -11,12 +13,16 @@ import { AuthService } from '../../common/service/auth.service';
 })
 export class LoginPageComponent {
 
-	loginForm: FormGroup; 
+	loginForm: FormGroup;
+
+	loggedInAsAdmin = false;
 
 	constructor(
 		private authService: AuthService,
 		private router: Router
 	) {
+		this.validateAdmin();
+
 		this.loginForm = new FormGroup({
 			username: new FormControl(undefined, [Validators.required]),
 			password: new FormControl(undefined, [Validators.required])
@@ -30,15 +36,48 @@ export class LoginPageComponent {
 					username: this.loginForm.controls['username'].value,
 					password: this.loginForm.controls['password'].value
 				};
-				this.authService.login(auth).subscribe(() => {
-					this.router.navigate(['/admin']);
+				this.authService.login(auth).subscribe({
+					next: () => {
+						this.router.navigate(['/admin']).finally(() => {
+							this.reloadWindow();
+						})
+					}
 				});
 			}
 		}
 	}
 	
-	isLogged(): boolean {
-		return this.authService.isLogged();
+	private validateAdmin() {
+		if (! this.authService.isTokenInLocalStorage()) {
+			this.unauthorizeUser();
+			return;
+		}
+
+		this.authService.validateToken().pipe(untilDestroyed(this)).subscribe({
+			next: (v) => {
+				if (v) {
+					this.loggedInAsAdmin = true;
+					this.router.navigate(['/login']);
+				} else {
+					this.unauthorizeUser();
+					this.reloadWindow();
+				}
+			},
+			error: () => {
+				this.unauthorizeUser();
+			}
+		});
 	}
 
+	private unauthorizeUser() {
+		this.removeTokenFromLocalStorage();
+	}
+
+	private removeTokenFromLocalStorage() {
+		this.authService.removeToken();
+	}
+
+	private reloadWindow() {
+		window.location.reload();
+	}
 }
